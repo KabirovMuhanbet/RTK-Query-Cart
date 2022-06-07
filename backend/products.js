0e3f028b87678258a284ab481f2aaf75c837220c
Muhanbet <muhanbetkabirov77@gmail.com>
2022-06-07 12:53:57 +0600

    first commit

---
+++ backend/products.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 13",
+        price: "790",
+        desc: "4.5-inch display",
+        image: "https://res.cloudinary.com/dpre7tisy/image/upload/v1653799346/SHOP/img_0_77_3127_4_1_v4gnpv.jpg"
+    },
+    {
+        id: 2,
+        name: "iPhone 13 Pro",
+        price: "890",
+        desc: "5.4-inch display",
+        image: "https://res.cloudinary.com/dpre7tisy/image/upload/v1653799346/SHOP/img_0_77_3156_0_1_swzkin.jpg"
+    },
+    {
+        id: 3,
+        name: "iPhone 13 Pro Max",
+        price: "990",
+        desc: "6.1-inch display",
+        image: "https://res.cloudinary.com/dpre7tisy/image/upload/v1653799343/SHOP/1-500x500_rq7m9z.jpg"
+    }
+]
+module.exports = products;
